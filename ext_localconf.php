9bd48550ac85b56cf2bcd0f562e4833042f57693
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-01-19 18:05:30 +0000

    [TASK] Remove tracks of pi1

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,17 +2,8 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tt_address_group = 1
 	options.saveDocNew.tt_address = 1
 ');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
-	$_EXTKEY,
-	'pi1/class.tx_ttaddress_pi1.php',
-	'_pi1',
-	'list_type',
-	1
-);
 
 if (TYPO3_MODE === 'BE') {
 	$settings = \TYPO3\TtAddress\Utility\SettingsUtility::getSettings();
@@ -21,6 +12,3 @@ if (TYPO3_MODE === 'BE') {
 			'TYPO3\\TtAddress\\Hooks\\DataHandler\\BackwardsCompatibilityNameFormat';
 	}
 }
-
-// Update scripts
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_group'] = 'TYPO3\\TtAddress\\Updates\\AddressGroupToSysCategory';

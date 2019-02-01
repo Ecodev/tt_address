5d8f4dc9b6b5434912b6903c8be8ec61bdf1ae02
Fabien Udriot <fabien@omic.ch>
2019-02-01 11:10:33 +0100

    Update TYPO3 dependency to be compatible with TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,6 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "tt_address"
-#
-# Auto generated 24-12-2007 00:40
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Address List',
 	'description' => 'Displays a list of addresses from an address table on the page.',
 	'category' => 'plugin',
@@ -19,20 +9,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'typo3@ingo-renner.com',
 	'author_company' => 'ingo-renner.com',
 	'version' => '3.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.2-7.9.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
+	'constraints' => [
+		'depends' => [
+			'typo3' => '6.2.2-8.7.99'
+        ],
+		'conflicts' => [
+        ],
+		'suggests' => [
 			'vidi' => '',
-		),
-	),
-	'suggests' => array(
-	),
+        ],
+    ],
+	'suggests' => [
+    ],
     'autoload' =>
-        array(
-            'classmap' => array('Classes')
-        ),
-);
+        [
+            'classmap' => ['Classes']
+        ],
+];

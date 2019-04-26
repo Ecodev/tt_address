f1a0fa92c3f4afb52e1c57a7954b0d21f297e046
Fabien Udriot <fabien@omic.ch>
2019-04-26 14:16:28 +0200

    Update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,10 @@ $EM_CONF[$_EXTKEY] = [
 	'description' => 'Displays a list of addresses from an address table on the page.',
 	'category' => 'plugin',
 	'state' => 'stable',
-	'author' => 'Ingo Renner',
-	'author_email' => 'typo3@ingo-renner.com',
-	'author_company' => 'ingo-renner.com',
 	'version' => '3.0.0-dev',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '6.2.2-9.7.99'
+			'typo3' => '9.5.0-9.5.99'
         ],
 		'conflicts' => [
         ],

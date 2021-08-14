d9b0d1ee63dab513ab462766c7692d7bc93d2ff2
Fabien Udriot <fabien@omic.ch>
2021-08-14 06:25:02 +0200

    Increase TYPO3 compatibility version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '3.0.0-dev',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0-9.5.99'
+			'typo3' => '9.5.0-10.4.00'
         ],
 		'conflicts' => [
         ],

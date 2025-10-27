a0da990dccb5d6b9c910b99ebdb2708a631c605b
Hyacinthe Compaoré <131278191+compaoreh338@users.noreply.github.com>
2025-10-27 11:24:33 +0000

    Update TYPO3 version dependency to 12.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '3.0.0-dev',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0-10.4.99'
+			'typo3' => '12.4.0-12.4.99',
         ],
 		'conflicts' => [
         ],
